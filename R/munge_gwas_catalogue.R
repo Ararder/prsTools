833babe676fc1427f00e9e4be29828a99aa701e3
Arvid Harder <arvid.harder@gmail.com>
2022-08-08 11:32:19 +0200

    ebi_to_ma now also returns chromosome and base pair, to make it easier for downstream analysis

--- R/munge_gwas_catalogue.R
+++ R/munge_gwas_catalogue.R
@@ -1,4 +1,4 @@
-utils::globalVariables(c("any_of", "freq"))
+utils::globalVariables(c("any_of", "freq", "chromosome", "position", "p2"))
 
 nonharmonized_ma <- c("SNP" = "variant_id",
   "A1" = "effect_allele",
@@ -23,6 +23,16 @@ harmonized_ma <- c("SNP" = "hm_rsid",
   "chromosome" = "hm_chrom")
 
 
+#' Select columns from gwas catalogue, to convert to .ma format from GCTA
+#'
+#' @param df dataframe downloaded from GWAS catalogue
+#'
+#' @return a tibble
+#' @export
+#'
+#' @examples \dontrun{
+#' select_cols(df)
+#' }
 select_cols <- function(df){
   if(any(stringr::str_detect(colnames(df), "^hm_"))) {
     dplyr::select(df, any_of(harmonized_ma))
@@ -72,8 +82,44 @@ ebi_to_ma <- function(df, n, snp_freq){
   df %>%
     select_cols() %>%
     check_n(n = n) %>%
-    check_freq(snp_freq = snp_freq) %>%
-    dplyr::select(., -any_of(c("position", "chromosome")))
+    check_freq(snp_freq = snp_freq)
+
+}
+
+
+get_top_mhc <- function(df, build = 38, pval = "p"){
+
+  if(build == 37){
+    mhc_start <- 28477797
+    mhc_end <- 33448354
+
+  } else if(build == 38){
+    mhc_start <- 28510120
+    mhc_end <- 33480577
+
+
+  }
+
+  mhc <- df %>%
+    dplyr::filter(chromosome == 6 & position >= mhc_start & position <= mhc_end)
+
+  if(is.character(df[["p"]])) {
+    message("The p-value column is a character vector,
+          this could mean P-values have are so small that they cannot be represented as doubles")
+    message("extracting the 3 last elements of the p-value string to determine smallest p-value")
+
+    mhc %>%
+      dplyr::mutate(p2 = as.numeric(.data[[pval]])) %>%
+      dplyr::filter(p2 == min(p2)) %>%
+      dplyr::mutate(p2 = stringr::str_sub(.data[[pval]], start = -3)) %>%
+      dplyr::filter(p2 == max(p2))
+
+  } else if(is.numeric(df[["p"]])) {
+
+    mhc %>%
+      dplyr::filter(.data[[pval]] == min(.data[[pval]]))
+  }
+
 
 }
 
